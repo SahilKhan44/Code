c3b50fe93f9f36a5cea28485a914d6d848bd8764
SahilKhan44 <sahil.khan44484@gmail.com>
2023-09-03 21:01:02 +0530

    first commit

---
+++ String/StringConcept.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // cout << "Enter your string" << endl;
+    // string str;
+    // getline(cin, str);
+    // cin >> str;
+    // we use for print with space
+
+    // cout << str << endl;
+
+    // // here we typeCast or print Ascii vale
+
+    // char ch = 'a';
+    // cout << int(ch);
+
+    /// Reverse function
+    cout << "Enter your string" << endl;
+    string str;
+    getline(cin, str);
+    // reverse(str.begin(), str.end());
+    // cout << str;
+}
